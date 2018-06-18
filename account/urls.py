529c31193ce759bea1ea993569f9a33a3cc5334b
rahul <officialrahulmandal@gmail.com>
2018-06-18 16:07:43 +0530

    added functionality for password change

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login')
+    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+
+    url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done')
 ]
